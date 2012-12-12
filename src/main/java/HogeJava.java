28db63632ea3f525d7bd2e8baccfbb1b793055f5
irof <irof@hogedriven.net>
2012-12-12 22:23:35 +0900

    add HogeJava

---
+++ src/main/java/HogeJava.java
@@ -0,0 +1,8 @@
+class HogeJava {
+    public String fizzBuzz(int i) {
+        if (i % 15 == 0) return "FizzBuzz";
+        if (i % 3 == 0) return "Fizz";
+        if (i % 5 == 0) return "Buzz";
+        return String.valueOf(i);
+    }
+}
