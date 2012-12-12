a26182c79de021b9901a7b520ef0cb2980bc8f72
irof <irof@hogedriven.net>
2012-12-12 23:18:21 +0900

    JUnitTest nested into HogeGroovyTest

--- src/test/groovy/HogeGroovyTest.groovy
+++ src/test/groovy/HogeGroovyTest.groovy
@@ -1,39 +1,44 @@
 import org.junit.Test
+import org.junit.experimental.runners.Enclosed
+import org.junit.runner.RunWith
 
 import static org.hamcrest.CoreMatchers.is
 import static org.junit.Assert.assertThat
 
+@RunWith(Enclosed)
 class HogeGroovyTest {
 
-    HogeGroovy sut = new HogeGroovy()
+    static class JUnitTest {
+        HogeGroovy sut = new HogeGroovy()
 
-    @Test
-    public void 三ならFizz() {
-        assertThat sut.fizzBuzz(3), is("Fizz")
-    }
+        @Test
+        public void 三ならFizz() {
+            assertThat sut.fizzBuzz(3), is("Fizz")
+        }
 
-    @Test
-    public void 三の倍数もFizz() {
-        assertThat sut.fizzBuzz(12), is("Fizz")
-    }
+        @Test
+        public void 三の倍数もFizz() {
+            assertThat sut.fizzBuzz(12), is("Fizz")
+        }
 
-    @Test
-    public void 五ならFizz() {
-        assertThat sut.fizzBuzz(5), is("Buzz")
-    }
+        @Test
+        public void 五ならFizz() {
+            assertThat sut.fizzBuzz(5), is("Buzz")
+        }
 
-    @Test
-    public void 五の倍数もFizz() {
-        assertThat sut.fizzBuzz(10), is("Buzz")
-    }
+        @Test
+        public void 五の倍数もFizz() {
+            assertThat sut.fizzBuzz(10), is("Buzz")
+        }
 
-    @Test
-    public void 十五ならFizzBuzz() {
-        assertThat sut.fizzBuzz(15), is("FizzBuzz")
-    }
+        @Test
+        public void 十五ならFizzBuzz() {
+            assertThat sut.fizzBuzz(15), is("FizzBuzz")
+        }
 
-    @Test
-    public void 十五の倍数もFizzBuzz() {
-        assertThat(sut.fizzBuzz(45), is("FizzBuzz"))
+        @Test
+        public void 十五の倍数もFizzBuzz() {
+            assertThat(sut.fizzBuzz(45), is("FizzBuzz"))
+        }
     }
 }
