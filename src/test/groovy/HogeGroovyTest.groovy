5228312d48827ce8a1cef362c4b94de4d460bcf7
irof <irof@hogedriven.net>
2012-12-13 00:06:05 +0900

    add TheoriesTest

--- src/test/groovy/HogeGroovyTest.groovy
+++ src/test/groovy/HogeGroovyTest.groovy
@@ -1,6 +1,9 @@
 import org.junit.Rule
 import org.junit.Test
 import org.junit.experimental.runners.Enclosed
+import org.junit.experimental.theories.DataPoints
+import org.junit.experimental.theories.Theories
+import org.junit.experimental.theories.Theory
 import org.junit.rules.TestRule
 import org.junit.rules.TestWatcher
 import org.junit.runner.RunWith
@@ -46,6 +49,24 @@ class HogeGroovyTest {
         @Test
         public void 十五の倍数もFizzBuzz() {
             assertThat(sut.fizzBuzz(45), is("FizzBuzz"))
+        }
+    }
+
+    @RunWith(Theories)
+    static class TheoriesTest {
+        HogeGroovy sut = new HogeGroovy()
+
+        @DataPoints
+        static Object[] params() {
+            def map = [:]
+            map << [3: "Fizz", 12: "Fizz"]
+            map << [5: "Buzz", 10: "Buzz"]
+            map << [15: "FizzBuzz", 30: "FizzBuzz"]
+        }
+
+        @Theory
+        public void test(def param) {
+            assert sut.fizzBuzz(param.key) == param.value
         }
     }
 }
