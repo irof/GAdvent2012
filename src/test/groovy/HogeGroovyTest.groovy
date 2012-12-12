877717ad854d4fbf6688da61c0bfd05da379ed3a
irof <irof@hogedriven.net>
2012-12-12 23:10:53 +0900

    remove Semicolon

--- src/test/groovy/HogeGroovyTest.groovy
+++ src/test/groovy/HogeGroovyTest.groovy
@@ -5,35 +5,35 @@ import static org.junit.Assert.assertThat
 
 class HogeGroovyTest {
 
-    HogeGroovy sut = new HogeGroovy();
+    HogeGroovy sut = new HogeGroovy()
 
     @Test
     public void 三ならFizz() {
-        assertThat(sut.fizzBuzz(3), is("Fizz"));
+        assertThat(sut.fizzBuzz(3), is("Fizz"))
     }
 
     @Test
     public void 三の倍数もFizz() {
-        assertThat(sut.fizzBuzz(12), is("Fizz"));
+        assertThat(sut.fizzBuzz(12), is("Fizz"))
     }
 
     @Test
     public void 五ならFizz() {
-        assertThat(sut.fizzBuzz(5), is("Buzz"));
+        assertThat(sut.fizzBuzz(5), is("Buzz"))
     }
 
     @Test
     public void 五の倍数もFizz() {
-        assertThat(sut.fizzBuzz(10), is("Buzz"));
+        assertThat(sut.fizzBuzz(10), is("Buzz"))
     }
 
     @Test
     public void 十五ならFizzBuzz() {
-        assertThat(sut.fizzBuzz(15), is("FizzBuzz"));
+        assertThat(sut.fizzBuzz(15), is("FizzBuzz"))
     }
 
     @Test
     public void 十五の倍数もFizzBuzz() {
-        assertThat(sut.fizzBuzz(45), is("FizzBuzz"));
+        assertThat(sut.fizzBuzz(45), is("FizzBuzz"))
     }
 }
